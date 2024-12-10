0104f3cc3ad0af385d4a24edda4ec7655cef85df
Adedoyin Sodiq <sodiqadedoyin12@gmail.com>
2024-12-10 09:09:57 +0100

    borrow with student id

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -28,48 +28,7 @@ class BookController {
         return $this->bookModel->delete($id);
     }
 
-    // public function borrowBook($id) {
-    
-    //     $book = $this->bookModel->getBookById($id);
-    //     if (!$book || $book['available_copies'] <= 0) {
-    //         return false; 
-    //     }
-
-        
-    //     $borrow_date = date("Y-m-d"); 
-    //     $return_date = date("Y-m-d", strtotime("+30 days")); 
-
-
-    //     return $this->bookModel->borrowBook($id, $borrow_date, $return_date);
-    // }
-    public function borrowBook($id, $return_date, $student_id, $student_name) {
-        $book = $this->bookModel->getBookById($id);
-    
-        if (!$book || $book['available_copies'] <= 0) {
-            return false; 
-        }
-    
-        $borrow_date = date("Y-m-d");
-    
-        // Borrow the book in the Book model
-        $isBookBorrowed = $this->bookModel->borrowBook($id, $borrow_date, $return_date);
-    
-        if ($isBookBorrowed) {
-            // Record transaction in the Transaction model
-            $transactionModel = new Transaction($this->bookModel->connect); // Assuming same connection
-            return $transactionModel->createTransaction([
-                'student_id' => $student_id,
-                'student_name' => $student_name,
-                'book_id' => $id,
-                'book_title' => $book['title'],
-                'borrow_date' => $borrow_date,
-                'return_date' => $return_date,
-                'status' => 'Borrowed'
-            ]);
-        }
-    
-        return false;
-    }
+  
     
 
     public function handleRequest() {
@@ -116,15 +75,7 @@ class BookController {
                 exit;
             }
 
-            if (isset($_POST['borrowBook'])) {
-                $id = $_POST['id'];
-                if ($this->borrowBook($id)) {
-                    header("Location: /lms_system/views/student/students.php?success=borrow");
-                } else {
-                    header("Location: /lms_system/views/student/book.php?error=borrow");
-                }
-                exit;
-            }
+          
         }
     }
 }
